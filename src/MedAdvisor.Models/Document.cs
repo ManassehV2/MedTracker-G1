de9017257c6255f09a19ff60368590bff1ad3759
hayat <hayattofik22@gmail.com>
2022-12-06 13:16:47 +0300

    Added DocumentModel/DataAccessLayer

--- src/MedAdvisor.Models/Document.cs
+++ src/MedAdvisor.Models/Document.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace MedAdvisor.Models
 {
-    internal class Document
+  public class Document
     {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+        public int user_id { get; set; }
     }
 }
