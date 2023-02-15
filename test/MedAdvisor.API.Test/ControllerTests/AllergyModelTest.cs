2110d2b8b2b9ba0d2432891d83f2db09402850fe
sichrisso <helenasolomon87@gmail.com>
2023-02-15 11:32:14 +0300

    added AllergyModel Test

---
+++ test/MedAdvisor.API.Test/ControllerTests/AllergyModelTest.cs
@@ -0,0 +1,21 @@
+﻿using MedAdvisor.Models;
+using NUnit.Framework;
+
+namespace MedAdvisor.API.Test.ControllerTests
+{
+    public class AllergyModelTest
+    {
+        [Test]
+        public void TestAllergyNameProperty()
+        {
+            // Arrange
+            var allergy = new Allergy();
+
+            // Act
+            var result = allergy.AllergyName;
+
+            // Assert
+            Assert.AreEqual(string.Empty, result);
+        }
+    }
+}
